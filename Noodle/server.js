bf223c4eab6a4578d9f39a999aa752d01c3640a5
bedangSen <35425072+bedangSen@users.noreply.github.com>
2018-11-08 13:04:46 +0400

    Add files via upload

---
+++ Noodle/server.js
@@ -0,0 +1,158 @@
+
+var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+var fs = require('fs');
+
+var visualRecognition = new VisualRecognitionV3({
+  version: '2018-03-19',
+  iam_apikey: 'gLr-_5z0bsp7igRRzrI7gnnN-xalLujHeM3fpu4cuADd'
+});
+
+var index = 0;
+
+// HTTP Portion
+var http = require('http');
+// URL module
+var url = require('url');
+var path = require('path');
+var Results = "";
+//
+// // Using the filesystem module
+// var fs = require('fs');
+
+var server = http.createServer(handleRequest);
+server.listen(8080);
+
+console.log('Server started on port 8080');
+
+function handleRequest(req, res) {
+  // What did we request?
+  var pathname = req.url;
+
+  // If blank let's ask for index.html
+  if (pathname == '/') {
+    pathname = '/index.html';
+  }
+
+  // Ok what's our file extension
+  var ext = path.extname(pathname);
+
+  // Map extension to file type
+  var typeExt = {
+    '.html': 'text/html',
+    '.js':   'text/javascript',
+    '.css':  'text/css'
+  };
+
+  // What is it?  Default to plain text
+  var contentType = typeExt[ext] || 'text/plain';
+
+  // User file system module
+  fs.readFile(__dirname + pathname,
+    // Callback function for reading
+    function (err, data) {
+      // if there is an error
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading ' + pathname);
+      }
+      // Otherwise, send the data, the contents of the file
+      res.writeHead(200,{ 'Content-Type': contentType });
+      res.end(data);
+    }
+  );
+}
+
+const WebSocket = require('ws');
+
+const server1 = new WebSocket.Server({ server:server });
+
+server1.on('connection', socket => {
+
+
+ // socket.send('Hello world!');
+
+ socket.addEventListener('message', event => {
+  //console.log(`Message from client: ${event.data}`)
+
+  if (`${event.data}`==='class')
+  {
+    visualRecognition.listClassifiers({
+    verbose: true
+    },
+    function(err, response) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(response);
+        socket.send(JSON.stringify(response, null, 2));
+
+      }
+    });
+  }
+  else
+    base64ToPNG(`${event.data}`, socket);
+
+});
+
+});
+
+// socket.addEventListener('message', event => {
+//   console.log(`Message from client: ${event.data}`)
+// });
+
+function base64ToPNG(data, socket) {
+
+  data = data.replace(/^data:image\/png;base64,/, '');
+
+  fs.writeFileSync(path.resolve(__dirname, 'Capture.PNG'), data, 'base64', function(err) {
+    if (err) throw err;
+  });
+
+
+  //sleep(1000);
+var images_file= fs.createReadStream(path.resolve(__dirname, 'Capture.PNG'));
+
+var owners = ["me"];
+var threshold = 0.8;
+
+var params = {
+  images_file: images_file,
+  owners: owners,
+  threshold: threshold
+};
+
+visualRecognition.classify(params, function(err, response) {
+  if (err)
+  {
+  	console.log(err);
+
+	//console.log(params);
+  }
+  else
+  {
+    //console.log(JSON.stringify(response, null, 2))
+    //console.log(params);
+
+    var JSON_obj = JSON.parse(JSON.stringify(response, null, 2));
+
+    if (JSON_obj.images[0].classifiers[0].classes.length > 0)
+    {
+	    console.log("Class: -->" + JSON_obj.images[0].classifiers[0].classes[0].class);
+	    Results = JSON_obj.images[0].classifiers[0].classes[0].class;
+    }
+    else
+	{
+		console.log("Was unable to identify the doodle. You suck! ");
+		Results = "0";
+
+	}
+
+	console.log("sending data ...");
+	socket.send(Results);
+}
+index++;
+});
+
+}
+
+module.exports = base64ToPNG;
---
+++ Noodle/sketch.js
@@ -0,0 +1,445 @@
+
+var drawing = [];
+var currentPath = [];
+var index = 0;
+var isDrawing = false;
+
+const timeLimit = 25;
+var x;
+// var socket= new WebSocket('ws://localhost:8080');
+var socket = new WebSocket('ws://noodle.mybluemix.net');
+var winSong;
+var loseSong;
+var congratulationsSong;
+
+var classes = [];
+var index = 0;
+var currentDraw = "";
+var previousDraw = "";
+var score = 0;
+
+var isEraser = true;
+var brushColor = 0;
+var brushSize = 4;
+
+// var classes = ["Airplane", "Dolphin", "Window", "Wine_Bottle", "Zebra", "Windmill","Wheelchair", "Fish"];
+
+// var classes = ["Airplane", "Alarm Clock", "Ant", "Ape", "Apple",
+// 			"Armor", "Axe", "Banana", "Bat", "Bear", "Bee",
+// 			"Beetle", "Bell", "Bench", "Bicycle", "Blimp", "Bread",
+// 			"Butterfly", "Dolphin", "Fish", "Wheelchair", "Windmill",
+// 			"Window", "Wine_Bottle", "Zebra"];
+
+function setup() {
+
+	const pigInsideLengths = [];
+	function setInsidePaths() {
+		let insidePaths = Array.from(document.querySelectorAll('g:not(.pig-inside) path'));
+		insidePaths.forEach((path) => {
+			const totLen = path.getTotalLength();
+			path.style.strokeDasharray = `0 ${totLen + 2}`;
+			path.style.strokeDashoffset = '1px';
+		});
+		insidePaths = Array.from(document.querySelectorAll('g.pig-inside path'));
+		insidePaths.forEach((path) => {
+			const totLen = path.getTotalLength();
+			path.style.strokeDasharray = `${totLen + 2} ${totLen + 2}`;
+			path.style.strokeDashoffset = '1px';
+			pigInsideLengths.push(totLen + 2.2);
+		});
+	}
+
+	const pig = document.querySelector('.pig')
+	function animate() {
+		setInsidePaths();
+		anime({
+			targets: '.pig + g path',
+			strokeDashoffset: [{
+				value: (_, i) => `${pigInsideLengths[i]}px`,
+				duration: 500,
+				easing: 'easeOutQuad',
+				delay: (_, i) => 1000 + i * 15
+			},
+												 {
+													 value: 0,
+													 duration: 500,
+													 easing: 'easeOutQuad',
+													 delay: (_, i) => 8700 + i * 15
+												 }
+												]
+		});
+		anime({
+			targets: '.pig',
+			strokeDashoffset: {
+				value: [0, -319],
+				duration: 1000,
+				easing: 'easeOutQuad',
+				delay: 1000
+			},
+			complete: () => {
+				pig.style.strokeDasharray = '0 320';
+				pig.style.strokeDashoffset = '1px';
+			}
+		});
+		anime({
+			targets: '.calculator + g path',
+			strokeDasharray: {
+				value: (el) => `${el.getTotalLength() + 1} ${el.getTotalLength() + 2}`,
+				duration: 500,
+				easing: 'easeOutQuad',
+				delay: (_, i) => 1400 + i * 15
+			},
+			strokeDashoffset: {
+				value: (el) => `${-el.getTotalLength() - 1}px`,
+				duration: 500,
+				easing: 'easeOutQuad',
+				delay: (_, i) => 4900 + i * 15
+			}
+		});
+		anime({
+			targets: '.calculator',
+			strokeDasharray: {
+				value: ['0 252', '252 252'],
+				easing: 'easeOutQuad',
+				duration: 1000,
+				delay: 1300
+			},
+			strokeDashoffset: {
+				value: [0, -252],
+				easing: 'easeOutQuad',
+				duration: 1000,
+				delay: 5300
+			}
+		});
+		anime({
+			targets: '.wallet',
+			strokeDasharray: {
+				value: ['0 240', '240 240'],
+				easing: 'easeOutQuad',
+				duration: 1000,
+				delay: 5600
+			},
+			strokeDashoffset: {
+				value: [0, -240],
+				easing: 'easeOutQuad',
+				duration: 1000,
+				delay: 9600
+			}
+		});
+		anime({
+			targets: '.wallet + g path',
+			strokeDasharray: {
+				value: (el) => `${el.getTotalLength() + 1} ${el.getTotalLength() + 2}`,
+				duration: 500,
+				easing: 'easeOutQuad',
+				delay: (_, i) => 5700 + i * 15
+			},
+			strokeDashoffset: {
+				value: (el) => `${-el.getTotalLength() - 1}px`,
+				duration: 500,
+				easing: 'easeOutQuad',
+				delay: (_, i) => 9200 + i * 15
+			}
+		});
+		setTimeout(() => {
+			anime({
+				targets: '.pig',
+				strokeDasharray: {
+					value: ['0 318', '319 319'],
+					easing: 'easeOutQuad',
+					duration: 1000,
+					delay: 900
+				}
+			});
+		}, 9000);
+	}
+
+	function schedule() {
+		animate();
+		setTimeout(schedule, 12900);
+	}
+	schedule();
+
+	score = 0;
+
+	socket.addEventListener('message', event => {
+		console.log("He is listening...");
+
+		console.log(`Message from server: ${event.data}`)
+
+		if(`${event.data}`.charAt(0) === '{')
+		{
+			console.log("classes are : ")
+			var JSON_obj = JSON.parse(`${event.data}`);
+			var classIndex = 0;
+			while(classIndex < JSON_obj.classifiers[0].classes.length)
+			{
+				classes[classIndex] = JSON_obj.classifiers[0].classes[classIndex].class;
+				classIndex++;
+			}
+			console.log(classes);
+		}
+
+		else if(`${event.data}`.charAt(0) === '0' || `${event.data}` != currentDraw )
+		{
+				// DEBUG: This is the wrong answer.
+				console.log("You guessed wrong!");
+
+				loseSong.play();
+		}
+
+		else
+		{
+			// DEBUG: This is the correct answer.
+			console.log("You guessed right!");
+
+			score++;
+			winSong.play();
+
+
+			console.log("Score : "  + score);
+			if (score == 5) {
+				$('.badgeContainer').parent().fadeIn(500);
+				$('.questionView').parent().fadeOut(0);
+				$('.topbar').parent().fadeIn(0);
+				$('.buttonHolder').parent().fadeOut(0);
+				stroke(0);
+				// document.getElementById('saveButton').fadeOut();
+			}
+		}
+
+		index = Math.floor((Math.random() * (classes.length-1)) + 1);
+		currentDraw = classes[index];
+		previousDraw = currentDraw;
+		console.log("Doodle : " + currentDraw);
+		document.getElementById('doodle_draw').innerHTML = currentDraw;
+		document.getElementById('doodle_draw_main').innerHTML = currentDraw;
+
+		console.log("SCORE : " + score);
+		document.getElementById('doodle_index').innerHTML = score;
+	});
+
+	socket.send('class');
+
+	document.getElementById('canvascontainer').style.cursor = "url('https://raw.githubusercontent.com/mcnitt/simple-jquery-drawing-app/master/img/cursor.png'), crosshair";
+
+	index++; // DEBUG: do we need this?
+	console.log("class length : " + classes.length);
+
+	index = Math.floor((Math.random() * (classes.length-1)) + 1);
+	currentDraw = classes[index];
+	previousDraw = currentDraw;
+	console.log("Doodle : " + currentDraw);
+	document.getElementById('doodle_draw').innerHTML = currentDraw;
+	document.getElementById('doodle_draw_main').innerHTML = currentDraw;
+
+	// Setting up canvas elements and other elements from the main page.
+	canvas = createCanvas(800, 600);
+	canvas.parent('canvascontainer');
+
+
+	canvas.mousePressed(startPath);
+	canvas.mouseReleased(endPath);
+
+	var saveButton = select('#saveButton');
+	saveButton.mousePressed(saveDrawing);
+
+	var clearButton = select('#clearButton');
+	clearButton.mousePressed(clearDrawing);
+
+	var skipButton = select('#eraser');
+	skipButton.mousePressed((function()
+	{
+		// location.reload();
+
+		console.log("IsEraser : " + isEraser);
+
+		if (isEraser) {
+			// NOTE: This is eraser
+			brushColor = 255;
+			brushSize = 25;
+
+			document.getElementById('eraser').innerHTML = "Pen";
+			document.getElementById('canvascontainer').style.cursor = "url('double-sided-eraser.png'), crosshair" ;
+			// document.getElementById("canvascontainer").style.cursor = "pointer";
+
+			isEraser = false;
+		}
+		else {
+			// NOTE: This is pen.
+			brushColor = 0;
+			brushSize = 4;
+
+			document.getElementById('eraser').innerHTML = "Eraser";
+			document.getElementById('canvascontainer').style.cursor = "url('https://raw.githubusercontent.com/mcnitt/simple-jquery-drawing-app/master/img/cursor.png'), crosshair";
+
+
+			isEraser = true;
+		}
+	}));
+
+	winSong = loadSound('win.mp3');
+	loseSong = loadSound('lose.mp3');
+
+
+	// console.log("This should be there only once.");
+	$('.topbar').parent().fadeOut();
+	$('.questionView').parent().fadeOut();
+	$('.badgeContainer').parent().fadeOut();
+
+	// Splashview button transition.
+	$('.spashviewButton').click(function() {
+		// console.log("Does this work?");
+
+		$('.splashscreen').parent().fadeOut(500);
+		$('.questionView').parent().fadeIn(500);
+		// startGame = true;
+	});
+
+	// Question View button transition.
+	$('.questionViewButton').click(function() {
+		$('.questionView').parent().fadeOut(500);
+		$('.topbar').parent().fadeIn(500, startTimer());
+	});
+
+
+}
+
+function startPath() {
+	isDrawing = true;
+	currentPath = [];
+	drawing.push(currentPath);
+}
+
+function endPath() {
+	isDrawing = false;
+}
+
+function draw(){
+	background(255);
+
+	if (isDrawing)
+	{
+		var point = {
+			x: mouseX,
+			y: mouseY,
+			c: brushColor,
+			s: brushSize
+		}
+
+		currentPath.push(point);
+		// console.log("Point: " + point.x + " " + point.y + " " + point.c );
+	}
+
+	// stroke(0);
+
+	// console.log('Stroke Color : ' + strokeColor);
+
+	// strokeWeight(4);
+	noFill();
+	for(var i = 0; i < drawing.length; i++){
+		var path = drawing[i];
+		beginShape();
+		for(var j = 0; j < path.length; j++){
+			stroke(path[j].c);
+			strokeWeight(path[j].s);
+			// console.log("path[j].s : " + path[j].s);
+			vertex(path[j].x, path[j].y)
+		}
+		endShape();
+	}
+}
+
+function saveDrawing()
+{
+	var canvas = $('canvas')[0];
+	data = canvas.toDataURL('image/png').replace(/data:image\/png;base64,/, '');
+
+	//var iname = 'img_' + index + '.png';
+
+	//post to php
+
+	// console.log("hello");
+	// socket.emit('check',"hi");
+	socket.send(data);
+
+	// socket.addEventListener('click', () => {
+	//   socket.send(data);
+	// });
+
+	clearDrawing();
+	clearInterval(x);
+
+	brushColor = 0;
+	brushSize = 4;
+
+	document.getElementById('eraser').innerHTML = "Eraser";
+	document.getElementById('canvascontainer').style.cursor = "url('https://raw.githubusercontent.com/mcnitt/simple-jquery-drawing-app/master/img/cursor.png'), crosshair";
+
+
+	isEraser = true;
+
+	loadQuestion();
+}
+
+function clearDrawing() {
+	drawing = [];
+
+}
+
+function deleteDrawing(key) {
+	var ref = database.ref(`drawings/${key}`).remove();
+}
+
+// Pads a zero in front of single digit numbers. This is for the time formatting.
+function pad(d) {
+    return (d < 10) ? '0' + d.toString() : d.toString();
+}
+
+function startTimer()
+{
+	// Set the date we're counting down to
+	var countDownTime = timeLimit;
+	document.getElementById("timeLeft").innerHTML = pad(countDownTime);
+	document.getElementById("clock-time").style.color = "rgb(69, 122, 190)";
+	// clearInterval(x);
+
+
+	// Update the count down every 1 second
+	x = setInterval(function() {
+		countDownTime--;
+
+		if (countDownTime/timeLimit > 0.75) {
+			document.getElementById("timeLeft").innerHTML = pad(countDownTime);
+			document.getElementById("clock-time").style.color = "rgb(69, 122, 190)";
+		}
+		else if (countDownTime/timeLimit > 0.5) {
+			document.getElementById("timeLeft").innerHTML = pad(countDownTime);
+			document.getElementById("clock-time").style.color = "rgb(228, 223, 95)";
+		}
+		else if (countDownTime/timeLimit > 0.25) {
+			document.getElementById("timeLeft").innerHTML = pad(countDownTime);
+			document.getElementById("clock-time").style.color = "rgb(255, 208, 87)";
+		}
+		else if (countDownTime/timeLimit > 0) {
+			document.getElementById("timeLeft").innerHTML = pad(countDownTime);
+			document.getElementById("clock-time").style.color = "rgb(244, 118, 106)";
+		}
+		else {
+			document.getElementById("timeLeft").innerHTML = pad(countDownTime);
+			document.getElementById("clock-time").style.color = "#EEEff0";
+			clearInterval(x);
+			// IDEA: Insert popup for next question here.
+			saveDrawing();
+		}
+	}, 1000);
+}
+
+function loadQuestion() {
+	$('.topbar').parent().fadeOut(500);
+	$('.questionView').parent().fadeIn(0);
+
+	// console.log("SCORE : " + score);
+	// document.getElementById('doodle_index').innerHTML = score + 1;
+	// document.getElementById('doodle_draw_main').innerHTML = currentDraw;
+}
